4014a5ea8b9c37878eee354cb99c4857bf3b0442
HAYASHITAKAYUKI <k021g1446@g.neec.ac.jp>
2022-04-25 16:32:15 +0900

    02_04_資料

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -58,4 +58,18 @@ class GameScene {
 	WorldTransform worldTransform_[100];
 	//カメラ上方向の角度
 	float viewAngle = 0.0f;
+
+	enum PartId 
+	{
+		Root,//大元
+		Spine,//脊椎
+		Chest,//胸
+		Head,//頭
+		ArmL,//左腕
+		ArmR,//右腕
+		Hip,//尻
+		LegL,//左足
+		LegR,//右足
+	};
+
 };
